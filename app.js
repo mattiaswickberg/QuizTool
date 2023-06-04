c6b8a7542612647ed0238e2734e6c25650a7e274
Mattias Wickberg <mattiaswickberg@gmail.com>
2023-06-04 13:05:23 +0200

    security fix, removed credentials from script

--- app.js
+++ app.js
@@ -1,34 +1,44 @@
 // server.js
-const express = require('express');
-const mariadb = require('mariadb');
-const cors = require('cors');
-const app = express();
-const port = process.env.PORT || 3000;
+const express = require('express')
+const mariadb = require('mariadb')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 3000
+const helmet = require('helmet')
+const fs = require('fs')
+
+let credentials = require('./credentials.json')
+let hostName = credentials.hostname
+let username = credentials.username
+let pw = credentials.password
 
 const pool = mariadb.createPool({
-     host: 'localhost', 
-     user:'testuser', 
-     password: 'test',
-     database: 'quiztool',
-     connectionLimit: 5
-});
+  host: hostName,
+  user: username,
+  password: pw,
+  database: 'quiztool',
+  connectionLimit: 5
+})
 
-app.use(cors());
+// Middleware
+app.use(cors())
+app.use(helmet());
 
 app.get('/quizzes', async (req, res) => {
-  let conn;
-  
+  let conn
+
+// Temporary, gets list of quizzes from database
   try {
-    conn = await pool.getConnection();
-    const rows = await conn.query("SELECT * FROM quizzes");
-    res.json(rows);
+    conn = await pool.getConnection()
+    const rows = await conn.query('SELECT * FROM quizzes')
+    res.json(rows)
   } catch (err) {
-    throw err;
+    throw err
   } finally {
-    if (conn) return conn.end();
+    if (conn) return conn.end()
   }
-});
+})
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+  console.log(`Server running at http://localhost:${port}`)
+})
