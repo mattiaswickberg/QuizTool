8f91c7d18230a4c3a96fc8ee52a8340e5dd88fd6
Mattias Wickberg <mattiaswickberg@gmail.com>
2023-06-05 14:54:40 +0200

    added css

--- frontend/src/components/Quiz.vue
+++ frontend/src/components/Quiz.vue
@@ -1,43 +1,47 @@
 <!-- frontend/src/components/Quiz.vue -->
 <template>
-    <div>
-      <h1>{{ quiz ? quiz.title : 'Loading...' }}</h1>
-      <p>{{ quiz ? quiz.description : '' }}</p>
-      <button @click="$router.push('/')">Back to list</button>
-  
-      <div v-if="quiz">
-        <form @submit.prevent="submitQuiz(quiz.quiz_id)">
-      <div v-for="question in questions" :key="question.question_id">
-        <h3>{{ question.question_text }}</h3>
-
-        <div v-for="option in question.options" :key="option.option_id">
-          <label>
-            <input
-              type="radio"
-              :name="question.question_id"
-              :value="option.option_id"
-              v-model="answers[question.question_id]"
-            >
-            {{ option.option_text }}
-          </label>
-        </div>
+    <div class="quiz">
+      <button @click="goBack">Back to list</button>
+      <div v-if="quiz && !quizFinished">
+        <h1>{{ quiz.title }}</h1>
+        <p>{{ quiz.description }}</p>
+          <div v-for="question in questions" :key="question.question_id" class="question">
+            <p>{{ question.question_text }}</p>
+            <div v-for="option in question.options" :key="option.option_id" class="options">
+    <label 
+      :class="{ 'option-selected': answers[question.question_id] === option.option_id, option: true }" 
+      @click="selectOption(question.question_id, option.option_id)"
+    >
+      <input 
+        type="radio" 
+        :name="question.question_id" 
+        :value="option.option_id" 
+        :checked="answers[question.question_id] === option.option_id"
+      >
+      {{ option.option_text }}
+    </label>
+  </div>
+          </div>
+          <button @click="submitQuiz">Submit</button>
       </div>
-
-      <button type="submit">Submit Quiz</button>
-    </form>
-      </div>
+      <QuizResult v-else-if="quizFinished" :score="score" :total="total" @back="goBack" />
     </div>
-  </template>
+</template>
   
   <script>
   import axios from 'axios';
+  import QuizResult from './QuizResult.vue';
 
   export default {
+    components: {
+    QuizResult,
+  },
     data() {
       return {
         quiz: null,
         questions: [],
         answers: {},
+        quizFinished: false,    
       };
     },
     async created() {
@@ -47,22 +51,75 @@
       this.questions = data.questions;
     },
     methods: {
-    async submitQuiz(quizId) {
-      try {
-        const response = await axios.post('http://localhost:3000/submit-quiz', {
-          quizId: quizId,
-          answers: this.answers,
-        });
-
-        // Display the result of the quiz
-        alert(`You scored ${response.data.score} out of ${response.data.total}!`);
-      } catch (err) {
-        console.error(err);
-      }
+        selectOption(questionId, optionId) {
+            this.answers = { ...this.answers, [questionId]: optionId };
+        },
+        
+        submitQuiz() {
+      axios.post('http://localhost:3000/submit-quiz', {
+        quizId: this.quiz.quiz_id,
+        answers: this.answers,
+      })
+      .then(response => {
+        this.score = response.data.score;
+        this.total = response.data.total;
+        this.quizFinished = true;
+      })
+      .catch(error => {
+        console.error(error);
+      });
     },
 
     // ...
   },
   };
   </script>
+  
+  <style scoped>
+.quiz {
+  margin: auto;
+  text-align: center;
+  border-radius: 5px;
+  max-width: 800px;
+  background-color: #333;
+  color: #fff;
+}
+
+.question {
+  margin-top: 20px;
+  font-size: 20px;
+}
+
+.options {
+  list-style: none;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+}
+
+.option {
+  margin: 10px 0;
+  background-color: #555;
+  padding: 10px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-align: left;
+  width: 100%;
+  box-sizing: border-box;
+}
+
+.option:hover {
+  background-color: #666;
+}
+
+.option-selected {
+  background-color: #4d3366;
+}
+
+input[type="radio"] {
+  display: none;
+}
+</style>
+
   
---
+++ frontend/src/components/QuizResult.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="quiz-result">
+      <h2>Your Score</h2>
+      <p>You got {{ score }} out of {{ total }} correct!</p>
+      <button @click="$emit('back')">Back to list</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['score', 'total'],
+  }
+  </script>
+  
+  <style scoped>
+.quiz-result {
+  max-width: 300px;
+  margin: auto;
+  text-align: center;
+  border-radius: 5px;
+  padding: 20px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+}
+
+button {
+  margin-top: 20px;
+  background-color: #008CBA; /* Blue */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #007B9A; /* Darker blue */
+}
+</style>
